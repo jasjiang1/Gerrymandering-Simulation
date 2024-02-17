566c3f8daa21bdd6ae3521db498ea4a2379b0380
ASaji77 <98294435+ASaji77@users.noreply.github.com>
2024-02-17 13:12:51 -0500

    Client Added

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import { MapContainer, TileLayer } from "react-leaflet";
+function App() {
+  return (
+    <MapContainer center = {[50.0000, 2.0000]} zoom = {13}>
+    </MapContainer>
+  );
+}
+
+export default App;
