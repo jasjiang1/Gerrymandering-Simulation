23c8189259336b0398b94ce2a691b4840a03d492
Jason Jiang <jasonjiang00000@gmail.com>
2024-02-26 18:44:39 -0500

    welcome page css, feel free to change colors

--- client/src/components/WelcomePage.js
+++ client/src/components/WelcomePage.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import logo from './grizzlies.png'
+import transparentlogo from './grizzliestransparent.png'
 
 function WelcomePage({ onStateSelected }) {
     return (
       <div className="welcome-modal">
         <h1>Team Grizzlies</h1>
-        <img src={logo} alt="Team Logo" />
-        <button onClick={() => onStateSelected('New Jersey')}>New Jersey</button>
-        <button onClick={() => onStateSelected('Georgia')}>Georgia</button>
+        <img className="welcomelogo" src={transparentlogo} alt="Team Logo" />
+        <div class="button-container">
+          <button onClick={() => onStateSelected('New Jersey')}>New Jersey</button>
+          <button onClick={() => onStateSelected('Georgia')}>Georgia</button>
+        </div>
       </div>
     );
   }
