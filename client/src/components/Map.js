65cc2e60a793c843b8a0d9e278ba63ca5ea5f61c
ASaji77 <98294435+ASaji77@users.noreply.github.com>
2024-03-13 13:29:16 -0400

    Added legend and summary table

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useRef } from 'react';
 import * as L from 'leaflet';
 import '../App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import NewJerseyCounties from '../mocks/NewJerseyCounties.json';
 import GeorgiaCounties from '../mocks/GeorgiaCounties.json';
 import GeorgiaState from '../mocks/GeorgiaState.json';
@@ -123,6 +124,26 @@ function Map({ mapSelection }) {
         }
       }
     }).addTo(mapRef.current);
+    var legend = L.control({position: 'topright'});
+    legend.onAdd = function (map) {
+    
+        var div = L.DomUtil.create('div', 'info legend');
+          var grades = [30, 20, 10, 10];
+      
+        // loop through our density intervals and generate a label with a colored square for each interval
+        for (var i = 0; i <grades.length; i++) { //rgb(0,174,243, opacites[i]);
+          div.innerHTML +=
+          '<i class="glyphicon glyphicon-stop" style="font-size:20px;  color:' + 'rgba(' + 0 + ',' + 173 + ',' + 243+ ',' + opacites[i] + ')' + '"></i>'+ (grades[i + 1] ? '+'+ grades[i] +'%'+'<br>' : '<'+grades[i] +'%');
+        }
+        div.style.backgroundColor="#f2f3f4";
+        div.style.fontSize="12px";
+        div.style.borderRadius = "5px"
+        div.style.borderColor= "gray";
+        div.style. borderStyle="solid";
+        return div;
+    };
+    
+    legend.addTo(mapRef.current);
 
     return () => {
       geoJsonLayer.remove();
@@ -156,19 +177,91 @@ function Map({ mapSelection }) {
 
   return(
     <div className="map-and-legend-container">
-      <div className="legend-container">
-        <span className="legend-text">Legend:</span>
-        <img src={legend} alt="Legend" className="map-legend"/>
-        <div className="tooltip-container">
-          <Tooltip title={<span className="custom-tooltip">{stateInfo}</span>} placement="right">
-            <IconButton>
-              <InfoIcon fontSize="large"/>
-            </IconButton>
-          </Tooltip>
-        </div>
-      </div>
-      <div ref={mapContainerRef} className="map-container"></div>
+    
+    <div ref={mapContainerRef} className="map-container"></div>
+    <div id = "table">
+      <table>
+        <tr><th id = "title" colspan="2">Summary of State Data</th></tr>
+        <tr>
+          <td>Total State Population</td>
+          {mapSelection.selectedState== "Georgia" && <td id ="state_pop" class="value">10,711,908</td>}
+          {mapSelection.selectedState== "New Jersey" && <td id ="state_pop" class="value">9,288,994</td>}
+        </tr>
+        <tr>
+        <th colspan="2">Population By Ethnicity</th>
+        </tr>
+        <tr>
+          <td id = "min_1">White </td>
+          {mapSelection.selectedState== "Georgia" && <td class="value">5,555,483</td>}
+          {mapSelection.selectedState== "New Jersey" && <td class="value">5,112,280</td>}
+        </tr>
+        <tr>
+          <td id = "min_1">Hispanic </td>
+          {mapSelection.selectedState== "Georgia" && <td class="value">1,123,457</td>}
+          {mapSelection.selectedState== "New Jersey" && <td class="value">2,002,575</td>}
+        </tr>
+        <tr >
+          <td id = "min_3">African American </td>
+          {mapSelection.selectedState== "Georgia" && <td class="value">3,320,513</td>}
+          {mapSelection.selectedState== "New Jersey" && <td class="value">1,219,770</td>}
+        </tr>
+        <tr >
+          <td id = "min_2">Asian </td>
+          {mapSelection.selectedState== "Georgia" && <td class="value">479,028</td>}
+          {mapSelection.selectedState== "New Jersey" && <td class="value">950,000</td>}
+        </tr>
+        <tr>
+          <td id = "min_4" >American Indian </td>
+          {mapSelection.selectedState== "Georgia" && <td class="value">50,618</td>}
+          {mapSelection.selectedState== "New Jersey" && <td class="value">51,186</td>}
+        </tr>
+        <tr>
+          <td id = "min_4" >Native Hawaiian </td>
+          {mapSelection.selectedState== "Georgia" && <td class="value">7,299</td>}
+          {mapSelection.selectedState== "New Jersey" && <td class="value">3,533</td>}
+        </tr>
+        <tr>
+          <td id = "min_4" >Other </td>
+          {mapSelection.selectedState== "Georgia" && <td class="value">555,059</td>}
+          {mapSelection.selectedState== "New Jersey" && <td class="value">1,048,641</td>}
+        </tr>
+        
+        <th colspan ="2">Demographics of State Assembly Members </th>
+        <tr>
+          <td id = "min_1"># of White House of Representatives </td>
+          {mapSelection.selectedState=="Georgia" && <td class="value">115</td>}
+          {mapSelection.selectedState== "New Jersey" && <td class="value">55</td>}
+        </tr>
+        <tr >
+          <td id = "min_3"># of African American House of Representatives </td>
+          {mapSelection.selectedState== "Georgia" && <td class="value">54</td>}
+          {mapSelection.selectedState== "New Jersey" && <td class="value">14</td>}
+        </tr>
+        <tr>
+          <td id = "min_1"># of Hispanic House of Representatives </td>
+          {mapSelection.selectedState== "Georgia" && <td class="value">2</td>}
+          {mapSelection.selectedState== "New Jersey" && <td class="value">8</td>}
+        </tr>
+        <tr >
+          <td id = "min_2"># of Asian House of Representatives </td>
+          {mapSelection.selectedState== "Georgia" &&  <td class="value">7</td>}
+          {mapSelection.selectedState== "New Jersey" &&  <td class="value">3</td>}
+        </tr>
+        <th colspan ="2">Politcal Parties of State Assmebly Members </th>
+        <tr>
+          <td id = "min_1">Number of Democratic Members  </td>
+          {mapSelection.selectedState== "Georgia" && <td class="value">77</td>}
+          {mapSelection.selectedState== "New Jersey" && <td class="value">50</td>}
+        </tr>
+        <tr>
+          <td id = "min_1">Number of Republican Members </td>
+
+          {mapSelection.selectedState== "Georgia" &&  <td class="value">101</td>}
+          {mapSelection.selectedState== "New Jersey" &&  <td class="value">30</td>}
+        </tr>
+      </table>
     </div>
+  </div>
   )
 }
 
