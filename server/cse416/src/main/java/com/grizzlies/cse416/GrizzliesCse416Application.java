a902f5a232aed7a1c277a8f1f81247bff62d5ecd
Lokesh V <lokesh.vvk@gmail.com>
2024-03-14 00:39:26 -0400

    created springboot server

---
+++ server/cse416/src/main/java/com/grizzlies/cse416/GrizzliesCse416Application.java
@@ -0,0 +1,13 @@
+package com.grizzlies.cse416;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrizzliesCse416Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrizzliesCse416Application.class, args);
+	}
+
+}
---
+++ server/cse416/src/test/java/com/grizzlies/cse416/GrizzliesCse416ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grizzlies.cse416;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrizzliesCse416ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
