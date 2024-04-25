81f86d2c36adb6fae30136b3af963dc800fdecca
Lokesh V <lokesh.vvk@gmail.com>
2024-04-25 01:19:24 -0400

    implementing cache

--- server/cse416/src/main/java/com/grizzlies/cse416/DemographicService.java
+++ server/cse416/src/main/java/com/grizzlies/cse416/DemographicService.java
@@ -3,6 +3,7 @@ package com.grizzlies.cse416;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.cache.annotation.Cacheable;
 
 @Service
 public class DemographicService {
@@ -10,10 +11,12 @@ public class DemographicService {
     @Autowired
     private District_Repository districtRepository;
 
+    @Cacheable(value = "districtPlans", key = "#state")
     public List<District_Data> getDistrictPlan(String state) {
         return districtRepository.findByPropertiesState(state);
     }
 
+    @Cacheable(value = "singleDistrict", key = "#state + '-' + #district")
     public District_Data getDistrict(String state, String district) {
         return districtRepository.findByPropertiesStateAndPropertiesDistrict(state, district);
     }
--- server/cse416/src/main/java/com/grizzlies/cse416/GrizzliesCse416Application.java
+++ server/cse416/src/main/java/com/grizzlies/cse416/GrizzliesCse416Application.java
@@ -3,9 +3,11 @@ package com.grizzlies.cse416;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
 @RestController
+@EnableCaching 
 public class GrizzliesCse416Application {
 
     public static void main(String[] args) {
